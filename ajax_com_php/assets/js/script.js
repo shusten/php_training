8e7e1f89b2c16638c7b31fb7fc31921e799e4771
Luis Villas-Boas <shusten@hotmail.com>
2024-04-30 23:48:28 -0300

    Testando formulário

--- ajax_com_php/assets/js/script.js
+++ ajax_com_php/assets/js/script.js
@@ -1,3 +1,11 @@
-$('#form1').on('submit', function(e){
+$('#form1')
+.on('submit', function(e){
     e.preventDefault();
+
+    var u_name = $('#name').val();
+    var u_comment = $('#comment').val();
+
+    console.log('Nome: ', u_name);
+    console.log('Comentário: ', u_comment);
+
 });
