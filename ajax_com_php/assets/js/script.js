e68614b06e881b469d75a496c394bbdf1f938257
Luis Villas-Boas <shusten@hotmail.com>
2024-05-01 13:13:51 -0300

    Monta requisição AJAX para testes

--- ajax_com_php/assets/js/script.js
+++ ajax_com_php/assets/js/script.js
@@ -8,4 +8,21 @@ $('#form1')
     console.log('Nome: ', u_name);
     console.log('Comentário: ', u_comment);
 
+    data = {
+        name: u_name,
+        comment: u_comment
+    };
+
+    $.ajax({
+        url: 'inserir.php',
+        method: 'POST',
+        data: data,
+        dataType: 'json',
+    })
+    .done(function(result){
+       alert('Resultado: ', result);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log('Requisição não concluída: ', textStatus, errorThrown);
+    });
 });
