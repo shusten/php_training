51b5f73f1df3b68fee7ee0c4f463e02f77b70a72
Luis Villas-Boas <shusten@hotmail.com>
2024-05-04 20:35:00 -0300

    :recycle: Refatorando: quebra código em funções.

--- ajax_com_php/assets/js/script.js
+++ ajax_com_php/assets/js/script.js
@@ -1,13 +1,19 @@
 const ENV = 'localhost:8081';
 
-$('#form1')
-.on('submit', function(e){
-    e.preventDefault();
+$(document).ready(function() {
+    $('#form1').on('submit', function(e) {
+        e.preventDefault();
+        addComment();
+    });
 
+    getComments();
+});
+
+function addComment() {
     var u_name = $('#name').val();
     var u_comment = $('#comment').val();
 
-    data = {
+    var data = {
         name: u_name,
         comment: u_comment
     };
@@ -18,29 +24,28 @@ $('#form1')
         data: data,
         dataType: 'json',
     })
-    .done(function(result){
-        $('#name').val('');
-        $('#comment').val('');
+    .done(function(result) {
+        clearForm();
         getComments();          
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
-        console.error('Requisição não concluída: ' + textStatus, errorThrown);
+        showError('Erro ao adicionar comentário. Por favor, tente novamente.');
     });
-});
+}
 
 function getComments() {
     $.ajax({
         url: `http://${ENV}/selecionar.php`,
         method: 'GET',
         dataType: 'json'
-    }).done(displayComments);
+    }).done(displayComments)
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        showError('Erro ao buscar comentário. Por favor, tete recarregar a página.');
+    });
 }
 
 function displayComments(comments) {
-    console.log(comments);
-
     comments.reverse();
-
     var commentHtml = comments.map(comment => `
         <div class="b_comm">
             <h4>${comment.name}</h4>
@@ -48,7 +53,14 @@ function displayComments(comments) {
         </div>
     `).join('');
 
-    $('.box_comment').prepend(commentHtml);
+    $('.box_comment').html(commentHtml);
 }
 
-getComments();
+function showError(message) {
+    alert(message);
+}
+
+function clearForm() {
+    $('#name').val('');
+    $('#comment').val('');
+}
