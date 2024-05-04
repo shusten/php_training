b034b9539f25c11c4f00321062c5cf4e1b172013
Luis Villas-Boas <shusten@hotmail.com>
2024-05-03 23:46:33 -0300

    adiciona selecionar.php para recuperar os dados do banco

--- ajax_com_php/selecionar.php
+++ ajax_com_php/selecionar.php
@@ -1,2 +1,16 @@
 <?php
- 
+
+  header('Content-Type: application/json');
+
+  require_once('./env.php');
+
+  $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+
+  $stmt = $pdo->prepare('SELECT * FROM comments');
+  $stmt->execute();
+
+  if ($stmt->rowCount() >= 1) {
+      echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+  } else {
+      echo json_encode('Nenhum comentário encontrado');
+  }
