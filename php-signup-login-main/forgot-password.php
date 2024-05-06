1b66c2277f58ca7796979d4368676e7e057a3fc4
Luis Villas-Boas <shusten@hotmail.com>
2024-05-05 23:36:50 -0300

    :construction: Writing the forgot-password.php file

---
+++ php-signup-login-main/forgot-password.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forgot Password</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+
+    <h1>Forgot Password</h1>
+
+</body>
+</html>
