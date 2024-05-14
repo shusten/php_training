f367bb4d3c44d48d879785f1d4bc92559448276a
Luis Villas-Boas <shusten@hotmail.com>
2024-05-13 22:22:56 -0300

    :hammer: Fix wrong route to reset password
    git push

--- php-signup-login-main/php_password_reset/send-password-reset.php
+++ php-signup-login-main/php_password_reset/send-password-reset.php
@@ -30,7 +30,7 @@ if ($mysqli->affected_rows) {
     $mail->Subject = "Password Reset";
     $mail->Body = <<<END
 
-    Click <a href="http://example.com/reset-password.php?token=$token">here</a> 
+    Click <a href="http://localhost:8081/reset-password.php?token=$token">here</a> 
     to reset your password.
 
     END;
